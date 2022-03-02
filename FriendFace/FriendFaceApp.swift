c6ee172198ff37a2dfccc12fba2ef54ddd12970c
George Mapaya <georit01@icloud.com>
2022-03-01 22:02:41 -0500

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by George Mapaya on 2022-03-01.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
